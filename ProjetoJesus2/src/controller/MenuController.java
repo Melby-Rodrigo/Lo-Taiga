6f48b39025cfb03924cd20b46c3e2c4e8877d237
Henrique Matos <henriquemundimmatos@gmail.com>
2024-11-02 02:31:47 -0300

    feat: crud

--- ProjetoJesus2/src/controller/MenuController.java
+++ ProjetoJesus2/src/controller/MenuController.java
@@ -4,7 +4,13 @@
  */
 package controller;
 
+import dao.Conexao;
+import dao.ProdutoDAO;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import view.MenuView;
+import model.Produto;
 
 /**
  *
@@ -12,11 +18,27 @@ import view.MenuView;
  */
 public class MenuController {
     private MenuView view;
-    public MenuController (MenuView view){
-    this.view = view;
+    private final ProdutoDAO dao;
+    
+    public MenuController (MenuView view) throws SQLException {
+        this.view = view;
+        Connection conexao = new Conexao().getConnection();
+        this.dao = new ProdutoDAO(conexao);
     }
     
-  
+    public Produto registrarProduto(String produto, String quantidade, String valor, String marca) {
+        return this.dao.insert(produto, quantidade, valor, marca);
+    } 
     
+    public ArrayList<Produto> listarProdutos() {
+        return this.dao.getAll();
+    }
     
+    public Produto atualizarProduto(int id, String produto, String quantidade, String valor, String marca) {
+        return this.dao.update(id, produto, quantidade, valor, marca);
+    }
+    
+    public int excluirProduto(int id) {
+        return this.dao.delete(id);
+    }
 }
--- ProjetoJesus2/src/dao/ProdutoDAO.java
+++ ProjetoJesus2/src/dao/ProdutoDAO.java
@@ -9,6 +9,9 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import model.Produto;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import mappers.ProductMapper;
 
 /**
  *
@@ -23,21 +26,97 @@ public class ProdutoDAO {
         this.connection = connection;
     }
     
-    public Produto insert(Produto produto) throws SQLException{
-        String sql = "insert into Produto(produto,valor,quantidade,marca) values(?,?,?,?) returning* ;";
-        PreparedStatement statement = connection.prepareStatement(sql);
-        statement.setString(1, produto.getProduto());
-        statement.setFloat(2, produto.getValor());
-        statement.setInt(3, produto.getQuantidade());
-        statement.setString(4, produto.getMarca());
-        statement.execute();
-        return null;
+    public Produto insert(String produto, String quantidade, String valor, String marca) {
+        String sql = "insert into produto (produto,valor,quantidade,marca) values(?,?,?,?) RETURNING *;";
+        ResultSet rs = null;
+        PreparedStatement statement = null;
         
-        
+        try {
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, produto);
+            statement.setFloat(2, Float.parseFloat(valor));
+            statement.setInt(3, Integer.parseInt(quantidade));
+            statement.setString(4, marca);
+
+            rs = statement.executeQuery();
+            
+            System.out.println(rs);
+            if (rs.next()) {
+                System.out.println(rs);
+                return ProductMapper.toEntity(rs);
+            }
+            
+            return null;
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     
-        
-        
     }
-       
     
+    public Produto update(int id, String produto, String quantidade, String valor, String marca) {
+        String sql = "update produto set produto = ?, marca = ?, quantidade = ?, valor = ? where id = ? RETURNING *;";
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        
+        try {
+            statement = connection.prepareStatement(sql);
+ 
+            statement.setString(1, produto);
+            statement.setString(2, marca);
+            statement.setInt(3, Integer.parseInt(quantidade));
+            statement.setFloat(4, Float.parseFloat(valor));
+            statement.setInt(5, id);
+            
+            rs = statement.executeQuery();
+            if (rs.next()) {
+                return ProductMapper.toEntity(rs);
+            }
+
+            return null;
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+    
+    public ArrayList<Produto> getAll() {
+        String sql = "SELECT * FROM produto;";
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        ArrayList<Produto> produtos = new ArrayList<>();
+        
+        try {
+            statement = connection.prepareStatement(sql);
+            rs = statement.executeQuery();
+            
+            while (rs.next()) {
+                produtos.add(ProductMapper.toEntity(rs));
+            }
+            
+            return produtos;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return produtos;
+        }
+    }
+    
+    public int delete(int id) {
+        String sql = "DELETE FROM produto WHERE produto.id = ?;";
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        ArrayList<Produto> produtos = new ArrayList<>();
+        
+        try {
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, id);
+            return statement.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
 }
